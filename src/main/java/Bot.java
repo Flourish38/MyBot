14d3e5fffe77d787bf4085952383e4bcb224e916
Ben <brsandvik@gmail.com>
2020-08-28 15:07:15 -0700

    Added NicknameCommand and it even works

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,6 +1,9 @@
 import listeners.*;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
 
 import javax.security.auth.login.LoginException;
 import java.io.File;
@@ -22,10 +25,14 @@ public class Bot {
             return;
         }
         JDA jda = JDABuilder.createDefault(new Scanner(file).nextLine())
+                .setChunkingFilter(ChunkingFilter.ALL)
+                .setMemberCachePolicy(MemberCachePolicy.ALL)
+                .enableIntents(GatewayIntent.GUILD_MEMBERS)
                 .addEventListeners(
                         new ReadyTimer(),
                         new PingCommand(),
-                        new ShutdownCommand()
+                        new ShutdownCommand(),
+                        new NicknameCommand()
                 ).build();
     }
 }
---
+++ src/main/java/listeners/NicknameCommand.java
@@ -0,0 +1,44 @@
+package listeners;
+
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NicknameCommand extends ParameterCommand {
+
+    public NicknameCommand() {
+        super("nickname", "nick");
+    }
+
+    @Override
+    void command(@NotNull GuildMessageReceivedEvent event, List<String> params) {
+        if(params.size() < 1){
+            event.getChannel().sendMessage("Usage: !nick <User> <Nickname...>").queue();
+            return;
+        }
+        String userId = params.get(0);
+        if(userId.startsWith("<@!"))
+            userId = userId.substring(3, userId.length() - 1);
+        if(!userId.matches("^\\d+$")) {
+            event.getChannel().sendMessage("Invalid user ID \"" + userId + "\" in first parameter.").queue();
+            return;
+        }
+        var member = event.getGuild().getMemberById(userId);
+        if(member == null){
+            event.getChannel().sendMessage("No member found with ID \"" + userId + "\"").queue();
+            return;
+        }
+        StringBuilder nick = new StringBuilder();
+        for(int i = 1; i < params.size(); i++){
+            nick.append(params.get(i));
+        }
+        if(member.isOwner()){
+            event.getChannel().sendMessage(member.getAsMention() + ": Change nick to \"" + nick.toString() + "\"").queue();
+            return;
+        }
+        member.modifyNickname(nick.toString()).queue();
+    }
+}
