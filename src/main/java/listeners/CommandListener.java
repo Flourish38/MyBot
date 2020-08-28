cbbe8f6ff9960e6cbb8b464e43585581d0c2817f
Ben <brsandvik@gmail.com>
2020-08-28 12:57:18 -0700

    Added varargs to commands for easy aliasing

--- src/main/java/listeners/CommandListener.java
+++ src/main/java/listeners/CommandListener.java
@@ -4,12 +4,19 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.annotation.Nonnull;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public abstract class CommandListener extends ListenerAdapter {
-    protected final String command;
+    protected final List<String> commands;
 
-    public CommandListener(String command){
-        this.command = command;
+    public CommandListener(String command, String... commands){
+        this.commands = Stream.concat(Stream.of(command), Arrays.stream(commands))
+                .sorted(Comparator.comparingInt(String::length).reversed())
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -17,11 +24,15 @@ public abstract class CommandListener extends ListenerAdapter {
         if (event.getAuthor().isBot()) return;
 
         var raw = event.getMessage().getContentRaw();
-
-        if((raw.startsWith(event.getJDA().getSelfUser().getAsMention() + " " + command)) // ping as prefix
-                || raw.startsWith(BotConfig.PREFIX + command)) // msg starts with prefix
+        String mention = event.getJDA().getSelfUser().getAsMention();
+        for(var command : commands)
         {
-            command(event);
+            if((raw.startsWith(mention + " " + command)) // ping as prefix
+                    || raw.startsWith(BotConfig.PREFIX + command)) // msg starts with prefix
+            {
+                command(event);
+                break;
+            }
         }
     }
 
--- src/main/java/listeners/ParameterCommand.java
+++ src/main/java/listeners/ParameterCommand.java
@@ -7,15 +7,18 @@ import java.util.Arrays;
 import java.util.List;
 
 public abstract class ParameterCommand extends CommandListener {
-    public ParameterCommand(String command) {
-        super(command);
+    public ParameterCommand(String command, String... commands) {
+        super(command, commands);
     }
 
     @Override
     void command(@Nonnull GuildMessageReceivedEvent event) {
         String raw = event.getMessage().getContentRaw();
-        raw = raw.substring(raw.indexOf(command) + command.length()).strip();
-        command(event, Arrays.asList(raw.split(" ")));
+        for(var command : commands){
+            raw = raw.substring(raw.indexOf(command) + command.length()).strip();
+            command(event, Arrays.asList(raw.split(" ")));
+            break;
+        }
     }
 
     abstract void command(@Nonnull GuildMessageReceivedEvent event, List<String> params);
